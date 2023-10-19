99a45ee77ee11d715ba9ec516bc775156f333d4e
Sonam Panwar <sonampanwar471@gmail.com>
2023-10-19 18:49:23 +0200

    setup authentication system to user login and sign the website, and design login and signup templates.

--- todo/models.py
+++ todo/models.py
@@ -1,33 +1,58 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.utils.translation import gettext_lazy as _
-from django.core.mail import send_mail
-from django.urls import reverse_lazy
 import enum
-from django.contrib.auth.validators import ASCIIUsernameValidator
 
 
-# Create your models here.
+# create your custom user manager.
+class CustomUserManager(BaseUserManager):
+    use_in_migrations = True
+    
+    def _create_user(self, email, password, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a regular User with the given email and password."""
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+# Create your custom user model.
 class CustomUser(AbstractUser):
-    username_validator = ASCIIUsernameValidator()
-    username = models.CharField(_("username"),
-                                max_length=150,
-                                unique=True,
-                                help_text=_("Required. letters, digits and @/./+/-/_ only."),
-                                validators=[username_validator],
-                                error_messages={
-                                    "unique": _("A user with that username already exists."),
-                                }, )
+    username = None
     email = models.EmailField(_("email address"),
                               unique=True,
                               error_messages={
                                   "unique": _("A user with that email address already exists."),
                               }, )
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
 
     def clean(self):
         self.email = self.__class__.objects.normalize_email(self.email).lower()
         super().clean()
-
 
 
 class TodoTaskStatus(enum.Enum):
--- todo/urls.py
+++ todo/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
-from todo.views import HomeView, LoginView, SignupView, AboutView
-
+from todo.views import HomeView, SignupView, AboutView, TodoboardView, LogoutView
+from django.contrib.auth.views import LoginView
+from todo.forms import LoginForm
 
 app_name = 'todo'
 
 urlpatterns = [
-    path('', HomeView.as_view(), name = 'Home'),
-    path('login/', LoginView.as_view(), name='Login'),
-    path('signup/', SignupView.as_view(), name='Signup'),
-    path('about/', AboutView.as_view(), name='About')
+    path('', HomeView.as_view(), name = 'HomePage'),
+    path('login/', LoginView.as_view(template_name='registration/login.html', form_class=LoginForm), name='LoginPage'),
+    path('logout/', LogoutView.as_view(), name='Logout'),
+    path('signup/', SignupView.as_view(), name='SignupPage'),
+    path('todoboard/', TodoboardView.as_view(), name='TodoboardPage'),
+    path('about/', AboutView.as_view(), name='AboutPage')
 ]
